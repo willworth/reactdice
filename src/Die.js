fbc6821a950606de50614793ae15df9ba387eb9f
Will Worth <will.worth@gmail.com>
2019-06-06 11:17:45 +0200

    first

---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Die extends Component {
+  render() {
+    return (
+      <div>
+        <h1>DIE</h1>
+      </div>
+    );
+  }
+}
+
+export default Die;
